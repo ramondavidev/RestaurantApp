c91bb46d8da3828633dc84e4f1c07473354aa5ce
Ramon <ramondavi@msn.com>
2023-12-24 10:07:03 -0300

    added firebase to the application and created navigation for accounts

--- App.js
+++ App.js
@@ -13,8 +13,22 @@ import { theme } from './src/infrastructure/theme';
 import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context';
 import { LocationContextProvider } from './src/services/location/location.context';
 import { FavouritesContextProvider } from './src/services/favourites/favourites.context';
+import { AuthenticationContextProvider } from './src/services/authentication/authentication.context';
 
 import { Navigation } from './src/infrastructure/navigation';
+
+import { initializeApp } from 'firebase/app';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: 'AIzaSyBf1n0TU1SxASBj8bBFaj9FmIQvcYcoYvk',
+  authDomain: 'mealstogo-26cc0.firebaseapp.com',
+  projectId: 'mealstogo-26cc0',
+  storageBucket: 'mealstogo-26cc0.appspot.com',
+  messagingSenderId: '193485309928',
+  appId: '1:193485309928:web:aa0ead9a33945936ff065d',
+};
+const app = initializeApp(firebaseConfig);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -32,13 +46,15 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavouritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavouritesContextProvider>
+        <AuthenticationContextProvider>
+          <FavouritesContextProvider>
+            <LocationContextProvider>
+              <RestaurantsContextProvider>
+                <Navigation />
+              </RestaurantsContextProvider>
+            </LocationContextProvider>
+          </FavouritesContextProvider>
+        </AuthenticationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
