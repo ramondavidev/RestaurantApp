cfa2607629964ad74104126edcdf594908e87f82
Ramon <ramondavi@msn.com>
2023-12-24 12:29:29 -0300

    buttons on account page

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,10 +1,32 @@
-import { View, Text } from 'react-native';
-import { AccountBackground, AccountCover } from '../components/account.styles';
+import { Spacer } from '../../../components/spacer/spacer.component';
+import {
+  AccountBackground,
+  AccountCover,
+  AccountContainer,
+  AuthButton,
+} from '../components/account.styles';
 
-export const AccountScreen = () => {
+export const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
+      <AccountContainer>
+        <AuthButton
+          icon="lock-open-outline"
+          mode="contained"
+          onPress={() => navigation.navigate('Login')}
+        >
+          Login
+        </AuthButton>
+        <AuthButton
+          icon="lock-open-outline"
+          mode="contained"
+          onPress={() => navigation.navigate('Register')}
+          marginTop="10px"
+        >
+          Register
+        </AuthButton>
+      </AccountContainer>
     </AccountBackground>
   );
 };
